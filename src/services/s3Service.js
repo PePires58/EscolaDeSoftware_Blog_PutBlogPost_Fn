1e1375bd47f83719d00c3b278267fb9c37b7f0e5
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-07-11 14:17:50 -0300

    fix s3service , key -> Key

--- src/services/s3Service.js
+++ src/services/s3Service.js
@@ -5,12 +5,9 @@ exports.PutObject = async function (objectKey) {
     const client = new S3Client({ region: process.env.Region });
     const command = new PutObjectCommand({
         Bucket: process.env.BlogPostContentBucketName,
-        key: objectKey,
+        Key: objectKey,
         Body: "/tmp/" + objectKey
     });
-
-    console.log("command:");
-    console.log(command);
 
     await client.send(command);
 

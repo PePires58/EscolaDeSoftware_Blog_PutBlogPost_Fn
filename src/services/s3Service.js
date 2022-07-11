fb2d4cca23fa05ee06c3e669297150028b8e503c
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-07-11 15:09:35 -0300

    update for timeout

--- src/services/s3Service.js
+++ src/services/s3Service.js
@@ -1,14 +1,35 @@
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 exports.PutObject = async function (objectKey, objectContent) {
-    const client = new S3Client({ region: process.env.Region });
-    const command = new PutObjectCommand({
-        Bucket: process.env.BlogPostContentBucketName,
-        Key: objectKey,
-        Body: objectContent
+
+    const errors = invalidObjectInput({
+        objectKey: objectKey,
+        objectContent: objectContent
     });
 
-    client.send(command);
+    if (errors) {
+        throw new Error(errors)
+    }
+    else {
 
-    return objectKey;
+        const client = new S3Client({ region: process.env.Region });
+        const command = new PutObjectCommand({
+            Bucket: process.env.BlogPostContentBucketName,
+            Key: objectKey,
+            Body: objectContent
+        });
+
+        client.send(command);
+    }
+}
+
+function invalidObjectInput(input) {
+    let errors = [];
+
+    if (!input.objectKey)
+        errors.push('object key is required');
+    else if (!input.objectContent)
+        errors.push('object content is required');
+
+    return errors;
 }

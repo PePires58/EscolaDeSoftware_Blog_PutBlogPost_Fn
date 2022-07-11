b1bb4906df7e4355c642bd7502dfd2840ff50206
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-07-11 13:42:25 -0300

    log into s3service

--- src/services/s3Service.js
+++ src/services/s3Service.js
@@ -5,18 +5,14 @@ exports.PutObject = async function (blogPostContent) {
 
     const objectKey = uuidv4();
 
-    console.log('object key');
-    console.log(objectKey);
-    console.log('end object key');
-
     const client = new S3Client({ region: process.env.Region });
     const command = new PutObjectCommand({
         Bucket: process.env.BlogPostContentBucketName,
         key: objectKey,
-        Body: blogPostContent,
-        ACL: 'private',
-        ContentEncoding: 'utf-8',
+        Body: blogPostContent
     });
+
+    console.log('command:' + command);
 
     await client.send(command);
 
